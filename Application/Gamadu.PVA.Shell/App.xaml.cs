220077c0f21018e3eebcded2a7652e352da5d6c9
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-01-15 12:54:23 +0100

    Initial commit and shell design
    The initial project commit and the first design of the shell window

---
+++ Application/Gamadu.PVA.Shell/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Gamadu.PVA.Shell
+{
+  using Prism.Ioc;
+  using Prism.Unity;
+  using System.Windows;
+
+  /// <summary>
+  /// Interaction logic for "App.xaml"
+  /// </summary>
+  public partial class App : PrismApplication
+  {
+    /// <summary>
+    /// Registers the types for dependency injection.
+    /// </summary>
+    /// <param name="containerRegistry"></param>
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+
+    /// <summary>
+    /// Creates the Shell window.
+    /// </summary>
+    /// <returns>The Shell window.</returns>
+    protected override Window CreateShell()
+    {
+      return Container.Resolve<Shell>();
+    }
+  }
+}
---
+++ Application/Gamadu.PVA.Shell/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Gamadu.PVA.Shell
+{
+  using System.Windows;
+
+  /// <summary>
+  /// Interaktionlogic for Shell.xaml
+  /// </summary>
+  public partial class Shell : Window
+  {
+    public Shell()
+    {
+      InitializeComponent();
+    }
+  }
+}
