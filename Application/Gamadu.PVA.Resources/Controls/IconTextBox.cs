b736875c679b6d439608fdce5e7030e318c1ab0a
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-04 12:50:07 +0100

    Add resources project for app themes and controls

---
+++ Application/Gamadu.PVA.Resources/Controls/IconTextBox.cs
@@ -0,0 +1,24 @@
+﻿namespace Gamadu.PVA.Resources.Controls
+{
+  using MaterialDesignThemes.Wpf;
+  using System.Windows;
+  using System.Windows.Controls;
+
+  public class IconTextBox : TextBox
+  {
+    static IconTextBox()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(IconTextBox), new FrameworkPropertyMetadata(typeof(IconTextBox)));
+    }
+
+    public PackIconKind Icon
+    {
+      get { return (PackIconKind)GetValue(IconProperty); }
+      set { SetValue(IconProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(PackIconKind), typeof(IconTextBox), new PropertyMetadata(default(PackIconKind)));
+  }
+}
---
+++ Application/Gamadu.PVA.Resources/ResourcesModule.cs
@@ -0,0 +1,20 @@
+﻿using Gamadu.PVA.Resources;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Gamadu.PVA.Resources
+{
+    public class ResourcesModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
