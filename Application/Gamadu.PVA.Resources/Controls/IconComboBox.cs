02125f17e428bc13d1e1f1fd0a50694a6ac77f66
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-05 10:23:25 +0100

    Create IconComboBox.cs

---
+++ Application/Gamadu.PVA.Resources/Controls/IconComboBox.cs
@@ -0,0 +1,24 @@
+﻿namespace Gamadu.PVA.Resources.Controls
+{
+  using MaterialDesignThemes.Wpf;
+  using System.Windows;
+  using System.Windows.Controls;
+
+  public class IconComboBox : ComboBox
+  {
+    static IconComboBox()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(IconComboBox), new FrameworkPropertyMetadata(typeof(IconComboBox)));
+    }
+
+    public PackIconKind Icon
+    {
+      get { return (PackIconKind)GetValue(IconProperty); }
+      set { SetValue(IconProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(PackIconKind), typeof(IconComboBox), new PropertyMetadata(default(PackIconKind)));
+  }
+}
