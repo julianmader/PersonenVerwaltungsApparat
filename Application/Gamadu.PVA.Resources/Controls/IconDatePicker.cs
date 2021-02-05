59eeca0d64f3d8987319d723be6e7bad309be2d9
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-05 10:23:22 +0100

    Create IconDatePicker.cs

---
+++ Application/Gamadu.PVA.Resources/Controls/IconDatePicker.cs
@@ -0,0 +1,24 @@
+﻿namespace Gamadu.PVA.Resources.Controls
+{
+  using MaterialDesignThemes.Wpf;
+  using System.Windows;
+  using System.Windows.Controls;
+
+  public class IconDatePicker : DatePicker
+  {
+    static IconDatePicker()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(IconDatePicker), new FrameworkPropertyMetadata(typeof(IconDatePicker)));
+    }
+
+    public PackIconKind Icon
+    {
+      get { return (PackIconKind)GetValue(IconProperty); }
+      set { SetValue(IconProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(PackIconKind), typeof(IconDatePicker), new PropertyMetadata(default(PackIconKind)));
+  }
+}
