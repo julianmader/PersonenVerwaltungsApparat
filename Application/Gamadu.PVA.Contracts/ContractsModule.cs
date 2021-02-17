09779ffbd23b8f5a73ee8a1657e309a3a8359200
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-17 22:59:48 +0100

    Add Contracts Module

---
+++ Application/Gamadu.PVA.Contracts/ContractsModule.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Gamadu.PVA.Contracts
+{
+  public class ContractsModule : IModule
+  {
+    public IRegionManager RegionManager { get; set; }
+
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+      this.RegionManager = containerProvider.Resolve<IRegionManager>();
+
+      this.RegionManager.RegisterViewWithRegion("ContractsRegion", typeof(Views.ContractsView));
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+
+    }
+  }
+}
---
+++ Application/Gamadu.PVA.Contracts/ViewModels/ContractsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamadu.PVA.Contracts.ViewModels
+{
+    public class ContractsViewModel : BindableBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public ContractsViewModel()
+        {
+            Message = "View A from your Prism Module";
+        }
+    }
+}
