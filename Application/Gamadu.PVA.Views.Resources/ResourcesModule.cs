fab0070cb526f021d6102730f5821c3ac9ae2c7b
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-18 23:21:56 +0100

    Add Resources View Module

---
+++ Application/Gamadu.PVA.Views.Resources/ResourcesModule.cs
@@ -0,0 +1,24 @@
+﻿using Gamadu.PVA.Views.Resources.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Gamadu.PVA.Views.Resources
+{
+    public class ResourcesModule : IModule
+    {
+    public IRegionManager RegionManager { get; set; }
+
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+      this.RegionManager = containerProvider.Resolve<IRegionManager>();
+
+      this.RegionManager.RegisterViewWithRegion("ResourcesRegion", typeof(Views.ResourcesView));
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+
+    }
+  }
+}
