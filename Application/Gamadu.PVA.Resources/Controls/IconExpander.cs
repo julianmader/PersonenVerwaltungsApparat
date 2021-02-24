be0d063d6c4347736dfddf103ede0fb1ffed306c
julianmader <55633496+julianmader@users.noreply.github.com>
2021-02-24 16:40:27 +0100

    Update Views

---
+++ Application/Gamadu.PVA.Resources/Controls/IconExpander.cs
@@ -0,0 +1,24 @@
+﻿namespace Gamadu.PVA.Resources.Controls
+{
+  using MaterialDesignThemes.Wpf;
+  using System.Windows;
+  using System.Windows.Controls;
+
+  public class IconExpander : Expander
+  {
+    static IconExpander()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(IconExpander), new FrameworkPropertyMetadata(typeof(IconExpander)));
+    }
+
+    public PackIconKind Icon
+    {
+      get { return (PackIconKind)GetValue(IconProperty); }
+      set { SetValue(IconProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(PackIconKind), typeof(IconExpander), new PropertyMetadata(default(PackIconKind)));
+  }
+}
