c782c50b8e3c6bac0582ed14d9bf0838e38ec531
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-24 22:48:35 +0100

    Create base class for identifiable objects

---
+++ Application/Gamadu.PVA.Business/Interfaces/IIdentifiable.cs
@@ -0,0 +1,15 @@
+﻿namespace Gamadu.PVA.Business.Interfaces
+{
+  public interface IIdentifiable
+  {
+    /// <summary>
+    /// Gets or sets the value for the ID.
+    /// </summary>
+    int ID { get; set; }
+
+    /// <summary>
+    /// Gets or sets the value for the Matchcode.
+    /// </summary>
+    string Matchcode { get; set; }
+  }
+}
---
+++ Application/Gamadu.PVA.Business/Models/IdentifiableBase.cs
@@ -0,0 +1,50 @@
+﻿namespace Gamadu.PVA.Business.Models
+{
+  using Gamadu.PVA.Business.Interfaces;
+  using Prism.Mvvm;
+  using System;
+
+  public abstract class IdentifiableBase : BindableBase, IEquatable<IdentifiableBase>, IComparable<IdentifiableBase>, IIdentifiable
+  {
+    /// <summary>
+    /// Backing field for <see cref="ID"/>.
+    /// </summary>
+    private int id;
+
+    /// <inheritdoc/>
+    public int ID
+    {
+      get { return this.id; }
+      set { this.SetProperty(ref this.id, value); }
+    }
+
+    /// <summary>
+    /// Backing field for <see cref="Matchcode"/>.
+    /// </summary>
+    private string matchcode;
+
+    /// <inheritdoc/>
+    public string Matchcode
+    {
+      get { return this.matchcode; }
+      set { this.SetProperty(ref this.matchcode, value); }
+    }
+
+    /// <inheritdoc/>
+    public int CompareTo(IdentifiableBase other)
+    {
+      if (this.ID < other.ID)
+      {
+        return -1;
+      }
+
+      return 1;
+    }
+
+    /// <inheritdoc/>
+    public bool Equals(IdentifiableBase other)
+    {
+      return this.ID == other.ID;
+    }
+  }
+}
