926d82833f7e9f85e4ef1219472b220a21e71726
julianmader <55633496+julianmader@users.noreply.github.com>
2021-02-25 09:24:27 +0100

    Add SQLite Module

---
+++ Application/Gamadu.PVA.Business.DataAccess.SQLite/SQLiteModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Gamadu.PVA.Business.DataAccess.SQLite
+{
+  public class SQLiteModule : IModule
+  {
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
