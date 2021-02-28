0386c4a2e21fcc46e631e54204b2ff12c8fa7707
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-02-28 23:28:01 +0100

    Add console project to test modules

---
+++ Application/TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TestConsole
+{
+  using Gamadu.PVA.Business.DataAccess;
+  using Gamadu.PVA.Business.DataAccess.MySQL;
+  using Gamadu.PVA.Business.Models;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IAllDataAccess dataAccess = new MySQLDataAccess("localhost", "db_pva", "root", string.Empty);
+      IEmployee employee = new Employee()
+      {
+        Matchcode = "Test3",
+        Forename = "Sven"
+      };
+
+      System.Console.WriteLine(dataAccess.SaveEmployee(employee)); 
+
+      System.Console.ReadLine();
+    }
+  }
+}
