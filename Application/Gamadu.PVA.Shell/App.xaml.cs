b085f42c01edc2a83d575d8701ea78384b8baf4a
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-03 13:17:42 +0100

    Change AutoViewModelLocator

--- Application/Gamadu.PVA.Shell/App.xaml.cs
+++ Application/Gamadu.PVA.Shell/App.xaml.cs
@@ -3,7 +3,11 @@
   using Gamadu.PVA.Shell.Views;
   using Prism.Ioc;
   using Prism.Modularity;
+  using Prism.Mvvm;
   using Prism.Unity;
+  using System;
+  using System.Globalization;
+  using System.Reflection;
   using System.Windows;
 
   /// <summary>
@@ -26,6 +30,34 @@
     protected override Window CreateShell()
     {
       return this.Container.Resolve<ShellView>();
+    }
+
+    protected override void ConfigureViewModelLocator()
+    {
+      base.ConfigureViewModelLocator();
+
+      ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+      {
+        var viewName = viewType.FullName;
+
+        if (viewName.Contains("Gamadu.PVA.Views."))
+        {
+          viewName = viewName.Replace("Gamadu.PVA.Views.", string.Empty);
+          viewName = viewName.Replace(".Views.", ".ViewModels.");
+
+          viewName = $"Gamadu.PVA.Views.{viewName}";
+        }
+        else
+        {
+          viewName = viewName.Replace(".Views.", ".ViewModels.");
+        }
+
+        var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+        var suffix = viewName.EndsWith("View") ? "Model" : "ViewModel";
+        var viewModelName = String.Format(CultureInfo.InvariantCulture, "{0}{1}, {2}", viewName, suffix, viewAssemblyName);
+        return Type.GetType(viewModelName);
+
+      });
     }
   }
 }
