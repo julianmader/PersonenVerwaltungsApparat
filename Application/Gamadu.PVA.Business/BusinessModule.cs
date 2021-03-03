faf58cd74fb63993b3a6db73e04c16f8ef1dc946
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-04 00:08:32 +0100

    Create BusinessModule.cs

---
+++ Application/Gamadu.PVA.Business/BusinessModule.cs
@@ -0,0 +1,19 @@
+﻿using Gamadu.PVA.Business.Models;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Gamadu.PVA.Business
+{
+  [Module(ModuleName = "BusinessModule")]
+  public class BusinessModule : IModule
+  {
+    public void OnInitialized(IContainerProvider containerProvider)
+    {
+    }
+
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+      containerRegistry.Register<IEmployee, Employee>();
+    }
+  }
+}
