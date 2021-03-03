d41e4ab79fa36a08aae01d17f28f95314fb25c80
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-03 13:18:38 +0100

    Extend view model

--- Application/Gamadu.PVA.Views.Employees/ViewModels/EmployeesViewModel.cs
+++ Application/Gamadu.PVA.Views.Employees/ViewModels/EmployeesViewModel.cs
@@ -1,19 +1,134 @@
-﻿using Prism.Mvvm;
+﻿using Gamadu.PVA.Business.DataAccess;
+using Gamadu.PVA.Business.Models;
+using Prism.Ioc;
+using Prism.Mvvm;
+using System.Collections.Generic;
 
 namespace Gamadu.PVA.Views.Employees.ViewModels
 {
   public class EmployeesViewModel : BindableBase
   {
-    private string _message;
-    public string Message
+    #region Properties
+
+    public IContainerProvider ContainerProvider { get; set; }
+
+    public IAllDataAccess DataAccess { get; set; }
+
+    private IEnumerable<IEmployee> availableEmployees;
+    public IEnumerable<IEmployee> AvailableEmployees
     {
-      get { return this._message; }
-      set { this.SetProperty(ref this._message, value); }
+      get { return availableEmployees; }
+      set { SetProperty(ref availableEmployees, value); }
     }
 
-    public EmployeesViewModel()
+    private IEnumerable<IDepartment> availableDepartments;
+    public IEnumerable<IDepartment> AvailableDepartments
     {
-      this.Message = "EmployeesView from your Prism Module";
+      get { return availableDepartments; }
+      set { SetProperty(ref availableDepartments, value); }
     }
+
+    private IEnumerable<IPosition> availablePositions;
+    public IEnumerable<IPosition> AvailablePositions
+    {
+      get { return availablePositions; }
+      set { SetProperty(ref availablePositions, value); }
+    }
+
+    private IEnumerable<IContract> availableContracts;
+    public IEnumerable<IContract> AvailableContracts
+    {
+      get { return availableContracts; }
+      set { SetProperty(ref availableContracts, value); }
+    }
+
+    private IEnumerable<IRoom> availableRooms;
+    public IEnumerable<IRoom> AvailableRooms
+    {
+      get { return availableRooms; }
+      set { SetProperty(ref availableRooms, value); }
+    }
+
+    private IEmployee selectedEmployee;
+    public IEmployee SelectedEmployee
+    {
+      get { return selectedEmployee; }
+      set { SetProperty(ref selectedEmployee, value); }
+    }
+
+    #endregion Properties
+
+    /// <summary>
+    /// Constructor for the Employees View Model
+    /// </summary>
+    public EmployeesViewModel(IContainerProvider container)
+    {
+      this.ContainerProvider = container;
+
+      this.SetDataAccess("MySQL");
+
+      this.RefreshAllData();
+    }
+
+    #region Methods
+
+    public void SetDataAccess(string identification = "")
+    {
+      this.DataAccess = this.ContainerProvider.Resolve<IAllDataAccess>(identification);
+    }
+
+    /// <summary>
+    /// Refreshes all data from the database.
+    /// </summary>
+    protected void RefreshAllData()
+    {
+      this.RefreshAvailableDepartments();
+      this.RefreshAvailablePositions();
+      this.RefreshAvailableContracts();
+      this.RefreshAvailableRooms();
+      this.RefreshAvailableEmployees();
+    }
+
+    /// <summary>
+    /// Gets all available employees from the database.
+    /// </summary>
+    protected void RefreshAvailableEmployees()
+    {
+      this.AvailableEmployees = DataAccess.GetEmployees();
+    }
+
+    /// <summary>
+    /// Gets all available departments from the database.
+    /// </summary>
+    protected void RefreshAvailableDepartments()
+    {
+      this.AvailableDepartments = DataAccess.GetDepartments();
+    }
+
+    /// <summary>
+    /// Gets all available positions from the database.
+    /// </summary>
+    protected void RefreshAvailablePositions()
+    {
+      this.AvailablePositions = DataAccess.GetPositions();
+    }
+
+    /// <summary>
+    /// Gets all available contracts from the database.
+    /// </summary>
+    protected void RefreshAvailableContracts()
+    {
+      this.AvailableContracts = DataAccess.GetContracts();
+    }
+
+    /// <summary>
+    /// Gets all available rooms from the database.
+    /// </summary>
+    protected void RefreshAvailableRooms()
+    {
+      this.AvailableRooms = DataAccess.GetRooms();
+    }
+
+    #endregion Methods
   }
 }
