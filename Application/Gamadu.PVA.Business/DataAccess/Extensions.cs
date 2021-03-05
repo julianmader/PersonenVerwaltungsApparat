cf8067d2e8956f6ab9ebee2bdd0f6719c885ec8b
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-05 16:21:32 +0100

    Add data access extensions

---
+++ Application/Gamadu.PVA.Business/DataAccess/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Gamadu.PVA.Business.DataAccess
+{
+  using Gamadu.PVA.Business.Models;
+
+  public static class Extensions
+  {
+    public static int SaveOrUpdateEmployee(this IAllDataAccess dataAccess, IEmployee employee)
+    {
+      if (employee.ID == null)
+      {
+        return dataAccess.SaveEmployee(employee);
+      }
+
+      return dataAccess.UpdateEmployee(employee);
+    }
+  }
+}
