7108560fa344b8e640a4aa4fca6d66447c2d8cc3
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-23 11:58:09 +0100

    Add CheckableEmployee model for dialogs

---
+++ Application/Gamadu.PVA.Business/Models/CheckableEmployee.cs
@@ -0,0 +1,17 @@
+﻿namespace Gamadu.PVA.Business.Models
+{
+  public class CheckableEmployee : Employee, ICheckableEmployee
+  {
+    /// <summary>
+    /// Backing field for <see cref="IsChecked"/>.
+    /// </summary>
+    private bool isChecked;
+
+    /// <inheritdoc/>
+    public bool IsChecked
+    {
+      get { return this.isChecked; }
+      set { this.SetProperty(ref this.isChecked, value); }
+    }
+  }
+}
