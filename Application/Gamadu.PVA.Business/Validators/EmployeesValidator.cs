387bd5f5754cd0971e0075b7ef00773079ceee7e
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-26 14:20:02 +0100

    Create EmployeesValidator.cs

---
+++ Application/Gamadu.PVA.Business/Validators/EmployeesValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Gamadu.PVA.Business.Validators
+{
+  using FluentValidation;
+  using Gamadu.PVA.Business.Models;
+
+  public class EmployeeValidator : AbstractValidator<Employee>
+  {
+    public EmployeeValidator()
+    {
+      this.RuleFor(x => x.Matchcode)
+        .NotEmpty();
+    }
+  }
+}
