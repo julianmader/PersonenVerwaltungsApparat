a64b9133481d1332efe378156383ab971cc0b739
Julian Mader <55633496+julianmader@users.noreply.github.com>
2021-03-26 14:20:10 +0100

    Create IValidateable.cs

---
+++ Application/Gamadu.PVA.Business/Models/IValidateable.cs
@@ -0,0 +1,18 @@
+﻿namespace Gamadu.PVA.Business.Models
+{
+  using FluentValidation;
+  using System.ComponentModel;
+
+  public interface IValidateable<T> : INotifyDataErrorInfo
+  {
+    /// <summary>
+    /// Gets or sets the validator used for object validation
+    /// </summary>
+    IValidator<T> Validator { get; set; }
+
+    /// <summary>
+    /// Validates the object
+    /// </summary>
+    void Validate();
+  }
+}
